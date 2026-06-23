9e32cd7396256b47ad6c4017d0f687d5b5eb91f3
ILHAM W <ilhamwahyudin365@gmail.com>
2026-06-23 12:38:50 +0700

    test: add feature tests for locale-aware routing and 404 redirection behavior

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -23,4 +23,20 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function test_404_redirects_to_root(): void
+    {
+        $response = $this->get('/invalid-page-does-not-exist');
+
+        $response->assertRedirect('/');
+    }
+
+    public function test_404_with_locale_redirects_to_locale_home(): void
+    {
+        $response = $this->get('/en/invalid-page-does-not-exist');
+        $response->assertRedirect(route('home.index', ['locale' => 'en']));
+
+        $responseId = $this->get('/id/invalid-page-does-not-exist');
+        $responseId->assertRedirect(route('home.index', ['locale' => 'id']));
+    }
 }
